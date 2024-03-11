f5ea06b6fab573c9faf2af840be2dd42fc2f4e5b
Edlandoe <Landoeliacin@gmail.com>
2024-03-11 13:21:38 -0400

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,26 +1,26 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const [startLine, ...Headers] = data.toString().trim().split("\r\n");
     const [method, path, version] = startLine.toString().trim().split(" ");
-    Headers.map((header, count) => {
-      count++;
-      console.log(`${count}: ${header}`)
+    Headers.map((header, index) => {
+      console.log(`${++index}: ${header}`)
     })
+
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (path.startsWith("/echo")) {
       let content = path.slice(6);
       let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
 
-      console.log(httpResponse);
       socket.write(httpResponse);
     } else if (path.startsWith("/user-agent")) {
-      let userAgent = 
-    } 
+      let userAgent = Headers[1].split(" ")[1];
+      let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+
+      socket.write(httpResponse)
+    }
     else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
