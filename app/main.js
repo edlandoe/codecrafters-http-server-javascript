f93ba74922f2fcbe1d61011ba725b55856f93f59
Edlandoe <Landoeliacin@gmail.com>
2024-03-11 15:15:57 -0400

    pass stage

--- app/main.js
+++ app/main.js
@@ -7,39 +7,52 @@ const server = net.createServer((socket) => {
     const [method, path, version] = startLine.toString().trim().split(" ");
     console.log(startLine);
 
-    if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (path.startsWith("/echo")) {
-      let content = path.slice(6);
-      let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+    switch (method) {
+      case "GET":
+        if (path === "/") {
+          socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else if (path.startsWith("/echo")) {
+          let content = path.slice(6);
+          let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
 
-      socket.write(httpResponse);
-    } else if (path.startsWith("/user-agent")) {
-      let userAgent = Headers[1].split(" ")[1];
-      let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+          socket.write(httpResponse);
+        } else if (path.startsWith("/user-agent")) {
+          let userAgent = Headers[1].split(" ")[1];
+          let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
 
-      socket.write(httpResponse);
-    } else if (path.startsWith("/files")) {
-      let filename = path.slice(7);
-      const fullPath = process.argv[3] + filename;
+          socket.write(httpResponse);
+        } else if (path.startsWith("/files")) {
+          let filename = path.slice(7);
+          const fullPath = process.argv[3] + filename;
 
-      console.log(`filename: ${filename}\nfullpath: ${fullPath}`);
+          if (fs.existsSync(fullPath)) {
+            let fileContent = fs.readFileSync(fullPath);
+            let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${Buffer.byteLength(
+              fileContent
+            )}\r\n\r\n${fileContent}`;
 
-      if (fs.existsSync(fullPath)) {
-        let fileContent = fs.readFileSync(fullPath);
-        let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${Buffer.byteLength(
-          fileContent
-        )}\r\n\r\n${fileContent}`;
+            console.log(httpResponse);
+            socket.write(httpResponse);
+          } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            socket.end();
+          }
+        } else {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+          socket.end();
+        }
+      case "POST":
+        if (path.startsWith("/files")) {
+          let filename = path.slice(7);
+          const directoryPath = process.argv[3];
+          const fileContent = fs.readFileSync(filename, "utf8");
 
-        console.log(httpResponse);
-        socket.write(httpResponse);
-      } else {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        socket.end();
-      }
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      socket.end();
+          fs.writeFileSync(directoryPath + filename, fileContent);
+          socket.write("HTTP/1.1 201 OK");
+        } else {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+          socket.end();
+        }
     }
   });
 
