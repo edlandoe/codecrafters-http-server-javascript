9931eb542583436a7a8951ec2bc2d30223f24848
Edlandoe <Landoeliacin@gmail.com>
2024-03-11 12:44:51 -0400

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,20 +1,25 @@
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log(data.toString().split(" "));
-    const [method, path, version] = data.toString().split(" ")
-    console.log(`method: ${method}\npath: ${path}\nversion: ${version}`)
+    const [startLine, ...Headers] = data.toString().trim().split("\r\n");
+    const [method, path, version] = startLine.toString().trim().split(" ");
 
     if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.startsWith("/echo")) {
+      content = path.slice(6);
+      httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+
+      console.log(httpResponse);
+      socket.write(httpResponse);
     } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
-  }) 
+  });
+
   socket.on("close", () => {
     socket.end();
     server.close();
