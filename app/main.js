b6198b59352053fb880744ded8188f514b91b2bd
Edlandoe <Landoeliacin@gmail.com>
2024-03-11 12:54:43 -0400

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,16 +6,22 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const [startLine, ...Headers] = data.toString().trim().split("\r\n");
     const [method, path, version] = startLine.toString().trim().split(" ");
-
+    Headers.map((header, count) => {
+      count++;
+      console.log(`${count}: ${header}`)
+    })
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (path.startsWith("/echo")) {
-      content = path.slice(6);
-      httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+      let content = path.slice(6);
+      let httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
 
       console.log(httpResponse);
       socket.write(httpResponse);
-    } else {
+    } else if (path.startsWith("/user-agent")) {
+      let userAgent = 
+    } 
+    else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
   });
